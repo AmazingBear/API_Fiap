140583a78a04999755a5f377963de29c898fc750
Vitor Dias <70549168+AmazingBear@users.noreply.github.com>
2021-11-08 09:31:52 -0300

    changes to Secret Key (again)

--- fiap/settings.py
+++ fiap/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-v_x%t*#+!jm298!686flgq$)w0)#7^cnu*nqn*qs+et&en5!w4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
